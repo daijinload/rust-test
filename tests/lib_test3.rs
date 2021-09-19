3db4e28a7ab9289f42d549d7baf04eae26775091
daijinload <daijinload@gmail.com>
2021-09-19 17:49:36 +0900

    aaa

--- tests/lib_test3.rs
+++ tests/lib_test3.rs
@@ -10,3 +10,12 @@ use rstest::*;
 fn あいうえお_test(#[case] input: u32, #[case] expected: u32) {
     assert_eq!(input, expected)
 }
+
+#[rstest(input, expected,
+    case(1, 1),
+    case(2, 2)
+)]
+fn かきくけこ_test(input: u32, expected: u32) {
+    println!("input:{}のとき、expectedは{}である", input, expected);
+    assert_eq!(expected, input);
+}

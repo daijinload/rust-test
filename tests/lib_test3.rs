66b306fcd435044e23e27df6c56b7e06908bfd88
daijinload <daijinload@gmail.com>
2021-09-19 17:52:36 +0900

    aaa

--- tests/lib_test3.rs
+++ tests/lib_test3.rs
@@ -2,6 +2,8 @@ extern crate rstest;
 
 use rstest::*;
 
+// RUST_BACKTRACE=1 RUSTFLAGS=-Awarnings cargo test --quiet --jobs=1 --no-fail-fast あいう -- --nocapture --show-output --test-threads=1
+
 #[rstest]
 #[case(0, 0)]
 #[case(1, 1)]
